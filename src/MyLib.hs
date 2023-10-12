c1f547ec83eb8e483bc1f9395c19402c97e8626b
alex800121 <alex800121@hotmail.com>
2023-10-13 03:12:48 +0800

    done state

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -59,18 +59,18 @@ class (Prj x xs (PrjF x xs), Inj x xs) => Member x xs
 instance (Prj x xs (PrjF x xs), Inj x xs) => Member x xs
 
 data Tele a where
-  PutStr :: String -> Tele ()
+  TellStr :: String -> Tele ()
   GetLine :: Tele String
 
 interpretTele :: Tele a -> IO a
-interpretTele (PutStr s) = Prelude.putStr s
+interpretTele (TellStr s) = Prelude.putStr s
 interpretTele GetLine = Prelude.getLine
 
 liftFree :: (Member f r) => f a -> FFree r a
 liftFree x = Impure (inj x) pure
 
-telePutStr :: (Member Tele r) => String -> FFree r ()
-telePutStr = liftFree . PutStr
+teleTellStr :: (Member Tele r) => String -> FFree r ()
+teleTellStr = liftFree . TellStr
 
 teleGetLine :: (Member Tele r) => FFree r String
 teleGetLine = liftFree GetLine
@@ -93,10 +93,10 @@ runReader i (Impure fx g) = case prj (Proxy :: Proxy (Reader i)) fx of
   Right r -> Impure r (runReader i <$> g)
 
 data Writer o x where
-  Put :: o -> Writer o ()
+  Tell :: o -> Writer o ()
 
-put :: (Member (Writer o) r) => o -> FFree r ()
-put = liftFree . Put
+tell :: (Member (Writer o) r) => o -> FFree r ()
+tell = liftFree . Tell
 
 runWriter ::
   forall o rs a.
@@ -105,17 +105,43 @@ runWriter ::
   FFree rs (o, a)
 runWriter (Pure x) = pure (mempty, x)
 runWriter (Impure fx g) = case prj (Proxy :: Proxy (Writer o)) fx of
-  Left (Put o) -> first (<> o) <$> runWriter (g ())
+  Left (Tell o) -> first (<> o) <$> runWriter (g ())
   Right r -> Impure r (runWriter <$> g)
+
+data State s x where
+  SGet :: Reader s x -> State s x
+  SPut :: Writer s x -> State s x
+
+runState ::
+  forall s rs a.
+  (Prj (State s) (State s ': rs) rs) =>
+  s ->
+  FFree (State s ': rs) a ->
+  FFree rs (s, a)
+runState s (Pure x) = pure (s, x)
+runState s (Impure fx g) = case prj (Proxy :: Proxy (State s)) fx of
+  Left (SGet Ask) -> runState s (g s)
+  Left (SPut (Tell o)) -> runState o (g ())
+  Right r -> Impure r (runState s <$> g)
+
+get :: (Member (State s) r) => FFree r s
+get = liftFree (SGet Ask)
+
+put :: (Member (State s) r) => s -> FFree r ()
+put = liftFree . SPut . Tell
+
+modify :: (Member (State s) r) => (s -> s) -> FFree r ()
+modify f = get >>= put . f
 
 run :: FFree '[] a -> a
 run (Pure x) = x
 run _ = undefined
 
 someFunc :: IO ()
--- someFunc = runFree runTele $ put =<< get
-someFunc = print $ run $ runReader (123 :: Int) $ runWriter @String $ do
+-- someFunc = runFree runTele $ tell =<< get
+someFunc = print $ run $ runState (456 :: Int) $ runWriter @String $ runReader (123 :: Int) $ do
   x <- ask @Int
-  put $ show x
-  put "Hello"
-  
+  y <- get @Int
+  tell $ show (x + y)
+  modify @Int (+ 10000)
+  tell "Hello"
